a26affd87daa9e413d8b70a0831abe6d4b0e0222
Jake Franklin <jacob.d.franklin@gmail.com>
2023-05-15 08:15:57 -0700

    Extend the math example

--- src/js/examples/math.js
+++ src/js/examples/math.js
@@ -11,10 +11,13 @@ const Ø = RIVEN.selector.bind(RIVEN);
 Ø("bang").create({x:4,y:8}, Node);
 
 // Int nodes
-Ø("add").create({x:14,y:4}, Add);
-Ø("int1").create({x:12,y:8}, Value, 3);
-Ø("int2").create({x:16,y:8}, Value, 5);
-Ø("print_int").create({x:20,y:4}, Print);
+Ø("add").create({ x: 14, y: 4 }, Add);
+Ø("int1").create({ x: 12, y: 8 }, Value, 3);
+Ø("int2").create({ x: 16, y: 8 }, Value, 5);
+Ø("print_int").create({ x: 20, y: 4 }, Print);
+Ø("add2").create({ x: 26, y: 4 }, Add);
+Ø("int3").create({ x: 26, y: 8 }, Value, 22);
+Ø("print_int2").create({ x: 32, y: 8 }, Print);
 
 // Str nodes
 Ø("concat").create({x:14,y:12}, Concat);
@@ -25,6 +28,9 @@ const Ø = RIVEN.selector.bind(RIVEN);
 Ø("bang").connect(["add","concat"]);
 Ø("add").connect(["print_int"]);
 Ø("add").syphon(["int1","int2"]);
+Ø("print_int").connect(["add2"]);
+Ø("add2").syphon(["int3"]);
+Ø("add2").connect(["print_int2"]);
 Ø("concat").syphon(["str1","str2"]);
 Ø("concat").connect(["print_str"]);
 
